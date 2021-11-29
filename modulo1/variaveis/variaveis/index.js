943bce7bd08e67107e8cc7e304ec04640440edf7
Vitorduarte0 <vitorsanta.cal.10@gmail.com>
2021-11-29 14:24:33 -0300

    exercicio-variaveis

---
+++ modulo1/variaveis/variaveis/index.js
@@ -0,0 +1,59 @@
+//exercício de interpretação//
+/*
+ 1-Questão
+A primeira saída vai ser 10 e a segunda saída vai ser 10 e 5.
+
+2-Questão
+A saída vai ser tudo 10
+
+3-Questão
+ let cargaHoraria = prompt("Quantas horas você trabalha por dia?")
+ let salarioEmDia = prompt("Quanto você recebe por dia?")
+ aleart(`Voce recebe ${cargaHoraria/salarioEmDia} por hora`)
+*/
+
+/*
+1-Questão de exercícios de codigo
+
+let nome = prompt("Digite seu nome");
+let idade = prompt("Digite sua idade");
+console.log("Olá", nome, "você tem" , idade, "anos");
+//Apareceu no console ""undefined", o que faz todo o sentido já que eu não atribuir nem um valor as respectivas variaveis.
+
+
+*/
+ // 2-Questão
+ /*let pergunta = "Voce gosta de chocolate";
+ console.log(pergunta, "- Sim");
+ let pergunta2 = "Voce gosta de djonga";
+ console.log(pergunta2, "- Sim");
+ let pergunta3 = "Voce torce para o flamengo";
+ console.log(pergunta3, "- Não");
+*/
+
+
+//3-Questão
+/*
+let a = 10;
+let b = 25;
+console.log(a);
+console.log(b);
+let c;
+c = a;
+a = b;
+b = c;
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+
+        //Desafio
+        
+/*
+let n1 = Number(prompt("Digite um numero"))
+let n2 = Number(prompt("Digite um segundo numero"))
+let x = n1 + n2;
+console.log(`A soma de ${n1} mais ${n2} é ${x}`)
+let y = n1*n2;
+console.log(`A multiplicação de ${n1} e ${n2} é ${y}`)
+*/
