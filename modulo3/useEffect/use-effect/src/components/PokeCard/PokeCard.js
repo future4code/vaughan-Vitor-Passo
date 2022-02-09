693b3c79e3cc6134cd6830f40c50bf432b762957
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-02-09 16:19:39 -0300

    subindo com implementação correta

--- modulo3/useEffect/use-effect/src/components/PokeCard/PokeCard.js
+++ modulo3/useEffect/use-effect/src/components/PokeCard/PokeCard.js
@@ -1,53 +1,21 @@
 
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
- const PokeCard = () =>{
+ const PokeCard = (props) =>{
 
     const [pokemon, setPokemon] = useState({})
 
   // método que roda após a montagem do componente
   useEffect(()=>{
-    // props.pokemon
-    // const pegaPokemon = pokeName => {
-    //   axios
-    //     .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    //     .then(response => {
-    //       // guarda as infos do pokemon no estado
-    //       // this.setState({ pokemon: response.data });
-    //       setPokemon(response.data)
-    //       console.log(pokemon)
-    //     })
-    //     .catch(err => {
-    //       console.log(err.response);
-    //     });
-    // };
-    // pegaPokemon(pokemon)
-    pegaPokemon()
+  
+    pegaPokemon(props.pokemons)
   }, [pokemon]) 
-    
-  // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-//   componentDidUpdate(prevProps) {
-//     // aqui, é feita uma verificação da props anterior com a props atual.
-//     // Caso a props anterior seja diferente da props atual,
-//     // a função pegaPokemon é chamada.
-//     if (prevProps.pokemon !== this.props.pokemon) {
-//       this.pegaPokemon(this.props.pokemon);
-//     }
-//   }
-
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
- 
+  
   const pegaPokemon = pokeName => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then(response => {
-        // guarda as infos do pokemon no estado
-        // this.setState({ pokemon: response.data });
+       
         setPokemon(response.data)
         console.log(pokemon)
       })
