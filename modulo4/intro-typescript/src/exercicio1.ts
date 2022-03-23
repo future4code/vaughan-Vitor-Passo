a945fd897ad7cdeeeb777c54f2b1ef5fd0f2de25
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-23 16:54:59 -0300

    subindo exercicio 1

---
+++ modulo4/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,19 @@
+//1)
+
+const checarTriangulo = (a: number, b:number, c:number) :string=> {
+    if(a !== b && b !== c){
+        return "Escaleno"
+    }
+    else if(a === b && b === c){
+        return "Equilátero"
+    }
+    else {
+        return "Isósceles"
+    }
+    
+}
+
+console.log(checarTriangulo(3, 3, 3 ))
+
+
+
