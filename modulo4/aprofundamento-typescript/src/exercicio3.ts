bb65555c6522e3bcae68401179bf1fb5ac73e484
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-24 21:12:36 -0300

    subindo questão 4

--- modulo4/aprofundamento-typescript/src/exercicio3.ts
+++ modulo4/aprofundamento-typescript/src/exercicio3.ts
@@ -1,30 +1,32 @@
-type Post ={
-    autor: string,
-    texto: string
-}
+// type Post ={
+//     autor: string,
+//     texto: string
+// }
 
-const posts  = [
-    {
-      autor: "Alvo Dumbledore",
-      texto: "Não vale a pena viver sonhando e se esquecer de viver"
-    },
-    {
-      autor: "Severo Snape",
-      texto: "Menos 10 pontos para Grifinória!"
-    },
-    {
-      autor: "Hermione Granger",
-      texto: "É levi-ô-sa, não levio-sá!"
-    },
-    {
-      autor: "Dobby",
-      texto: "Dobby é um elfo livre!"
-    },
-    {
-      autor: "Lord Voldemort",
-      texto: "Avada Kedavra!"
-    }
-  ]
-//   const buscarPostsAutor(post, autoInformado) =>{
-
+// const Posts  = [
+//     {
+//       autor: "Alvo Dumbledore",
+//       texto: "Não vale a pena viver sonhando e se esquecer de viver"
+//     },
+//     {
+//       autor: "Severo Snape",
+//       texto: "Menos 10 pontos para Grifinória!"
+//     },
+//     {
+//       autor: "Hermione Granger",
+//       texto: "É levi-ô-sa, não levio-sá!"
+//     },
+//     {
+//       autor: "Dobby",
+//       texto: "Dobby é um elfo livre!"
+//     },
+//     {
+//       autor: "Lord Voldemort",
+//       texto: "Avada Kedavra!"
+//     }
+//   ]
+//   function buscarPostsAutor(posts: string[], autoInformado: string): string[] {
+//       return posts.filter((post)=>{
+//         return post.autor === autoInformado
+//       })
 //   } 
