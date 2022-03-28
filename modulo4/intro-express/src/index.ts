591bdc08968da8de5ea6fdeab663ccb575740ddf
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-28 17:18:00 -0300

    subindo exercicio 5

--- modulo4/intro-express/src/index.ts
+++ modulo4/intro-express/src/index.ts
@@ -21,6 +21,32 @@ type Tipo = {
     email: string,
     website: string
 }
+
+type Posts = {
+    id: string | number,
+    title: string,
+    body: string,
+    userId: string | number
+}
+
+let post: Posts[] = [
+    
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "qui est esse",
+          "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+        
+]
+
+
 let usuario: Tipo[] = [
     {
         id: "12", 
@@ -46,5 +72,9 @@ let usuario: Tipo[] = [
 ]
 
 app.get("/users", ((req, res)=>{
-    res.status(200).send(usuario)
+    res.status(220).send(usuario)
+}))
+
+app.get("/posts", ((req, res)=>{
+    res.status(215).send(post)
 }))
