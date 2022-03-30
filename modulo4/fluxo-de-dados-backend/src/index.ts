06364b6caf4029c8c551cdb3c0ffbfb1755a56ae
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-30 14:19:36 -0300

    subindo exercicio 1

---
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(express.json)
+app.use(cors())
+
+app.get("/test", (req, res)=>{
+    res.send("API funcionando e rodando na porta 3003")
+})
+
+app.listen(3003, ()=>{
+    console.log("Back end rodando na porta 3003")
+})
