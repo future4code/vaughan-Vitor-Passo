c43b3f65c12383c27d1394d4d0e915a8b2b37ea6
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-30 14:50:02 -0300

    subindo exercicio 2

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -10,6 +10,17 @@ app.get("/test", (req, res)=>{
     res.send("API funcionando e rodando na porta 3003")
 })
 
+app.post("/createProduct", (req, res)=>{
+    
+    const addProduct = {
+        id: '6',
+        name: req.body.name,
+        price: req.body.price
+    }
+    product.push(addProduct)
+    res.status(201).send(product)
+})
+
 app.listen(3003, ()=>{
     console.log("Back end rodando na porta 3003")
 })
