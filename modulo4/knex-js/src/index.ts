d2c85d9509899a42be5adc239f82a9843bd8863d
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-07 23:04:56 -0300

    subindo criacao de atores

--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -7,6 +7,40 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+//2) a)
+
+const updateSalary = async (id: string, salary: number): Promise <any> => {
+    await connection("actor").update({salary: salary}).where("id", id)
+}
+
+const deleActor = async (id: string) => {
+    await connection('actor').delete().where("id", id)
+}
+const avgSalary = async (gender: string): Promise<any> => {
+    const result = await connection("Actor")
+      .avg("salary as average")
+      .where({ gender });
+
+    return result[0].average;
+  };
+
+type Actor = {
+    id: number,
+    nome: string,
+    salary: number,
+    gender: string
+}
+const createActor = async (ator: Actor) :Promise<any> => {
+    // try {
+    //     await connection
+    // } catch (e: any) {
+    //     console.log(e.message)
+    // }
+
+}
+
+
 app.get("/Actor", async(req, res) : Promise <void> =>{
         try {
             const resultado = await connection("actor")
@@ -19,7 +53,7 @@ app.get("/Actor", async(req, res) : Promise <void> =>{
 
 app.get("/name/:name", async(req, res) : Promise <void> =>{
     try {
-        
+
         const name = req.params.name
         if (!name) {
             throw new Error("Insira um valor válido")
@@ -34,12 +68,12 @@ app.get("/name/:name", async(req, res) : Promise <void> =>{
             case "Insira um valor válido":
                 res.status(403).send(e.message)
                 break;
-            
+
             default:
                 res.status(500).send(e.message)
                 break;
         }
-        
+
     }
 })
 
@@ -56,6 +90,23 @@ app.get("/count", async (req, res)=>{
 
     }
 
+})
+
+app.post("/createActor", async (req, res)=>{
+    try {
+        const {id, nome, salary, gender} = req.body
+        const actor = await connection("actor").insert(createActor(
+            {id,
+             nome, 
+             salary, 
+             gender}
+        ))
+        console.log(actor)
+        res.status(201).send(actor)
+        
+    } catch (e: any) {
+        res.status(404).send(e.message)
+    }
 })
 
 const server = app.listen(process.env.PORT || 3003, () => {
