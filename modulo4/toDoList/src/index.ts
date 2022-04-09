c3a4e3b36d36d20a318a868648a634205e99f43f
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-09 00:57:43 -0300

    implementando criação de tarefa, mas ainda falta ajustar

--- modulo4/toDoList/src/index.ts
+++ modulo4/toDoList/src/index.ts
@@ -44,29 +44,44 @@ app.post('/createUser' , async(req, res)=>{
     }
 })
 
+app.post('/task', async (req, res): Promise <void> => {
+    try {
+        let id = Date.now().toString()
+        let title = req.body.title
+        let description = req.body.description
+        let status = req.body.status
+        let limit_date = Date.now().toString()
+        let creator_user_id = Date.now().toString()
+        const result = await connection('task').insert({
+            id, title, description, status, limit_date, creator_user_id
+        })     
+        res.status(201).send(limit_date)
+        console.log(limit_date)
+    } catch (e: any) {
+        switch (e.message) {
+            case "value":
+                res.status(401).send(e.message)
+                break;
+            default:
+                res.status(500).send(e.message)
+                break;
+        }
+    }
+    
+})
+
 app.put('/editUSer/:id', async(req, res): Promise <void> =>{
     try {
         let name = req.body.name
         let nickName = req.body.nickName
         let email = req.body.email  
-
-        if (!name || !nickName || !email ) {
-            throw new Error("Favor preecha todos os campos!")
-        }
             await connection('TodoListUser').update({
                 name, nickName, email
             }).where({id: req.params.id})
             res.status(201).send({message: "Usuario editado com sucesso!"})
             
     } catch (e: any) {
-        switch (e.message) {
-            case "Favor preecha todos os campos!":
-                res.status(422).send(e.message)
-                break;
-            default:
-                res.status(500).send(e.sqlMessage || e.message)
-                break;
-        }
+        res.status(500).send(e.sqlMessage || e.message)
     }
 })
 
@@ -77,12 +92,27 @@ app.get('/user',async (req, res)=> {
 })
 
 app.get('/user/:id', async (req, res)=>{
-    const id = req.params.id
-    const result = await connection.raw(`
-        SELECT * FROM TodoListUser WHERE id = "${id}"
-    `)
-    
-    res.status(201).send(result[0][0])
+
+    try {
+        const id = req.params.id
+        if(!id){
+            throw new Error("Insira um id válido");
+        }
+        const result = await connection.raw(`
+            SELECT * FROM TodoListUser WHERE id = "${id}"
+        `)
+        res.status(201).send(result[0][0])
+    } catch (e: any) {
+        switch (e.message) {
+            case "Insira um id válido":
+                res.status(422).send(e.message)
+                break;
+        
+            default:
+                res.status(500).send(e.sqlMessage || e.message)
+                break;
+        }
+    }
 })
 app.listen(3003, ()=>{
     console.log("Back end rodando na porta 3003")
