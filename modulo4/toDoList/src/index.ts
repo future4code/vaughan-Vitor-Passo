9f9333f72706dac4651e9835c1c62d287be3bbc5
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-11 00:10:47 -0300

    subindo mudancas, mas faltas algumas implementações e algumas mudanças

--- modulo4/toDoList/src/index.ts
+++ modulo4/toDoList/src/index.ts
@@ -6,12 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const getId = async (id:string): Promise <void> => {
-// new Date().toISOString() criar data e hora
-//
-// }
-
-
 // esse endPoint cria um novo usuário
 app.post("/createUser", async (req, res) => {
   try {
@@ -43,6 +37,7 @@ app.post("/createUser", async (req, res) => {
     }
   }
 });
+
 // esse endPoint cria uma nova task
 app.post("/task", async (req, res): Promise<void> => {
   try {
@@ -52,7 +47,7 @@ app.post("/task", async (req, res): Promise<void> => {
     let limit_date = req.body.limit_date;
     let creator_user_id = req.body.creator_user_id;
 
-    if(!title && !description && !limit_date && !creator_user_id){
+    if (!title && !description && !limit_date && !creator_user_id) {
       throw new Error("Favor preecha os campos!");
     }
     await connection("task").insert({
@@ -68,10 +63,10 @@ app.post("/task", async (req, res): Promise<void> => {
       limit_date,
       creator_user_id
     });
-  } catch (e: any) {
+  } catch (e) {
     switch (e.message) {
       case "Favor preecha os campos!":
-        res.status(401).send({message: e.message});
+        res.status(401).send({ message: e.message });
         break;
       default:
         res.status(500).send({ message: e.sqlMessage || e.message });
@@ -80,6 +75,28 @@ app.post("/task", async (req, res): Promise<void> => {
   }
 });
 
+// "responsible_user_id": null
+app.post('/test', async (req, res): Promise<void> => {
+  try {
+    let task_id = req.body.task_id;
+    let responsible_user_id = req.body.responsible_user_id;
+    if (!task_id || !responsible_user_id) {
+      throw new Error("Campos vazios, Favor preencha");
+    }
+    await connection('ResponsibleUserTaskRelation')
+    .insert({task_id, responsible_user_id})
+    res.status(201).send({task_id, responsible_user_id})
+  } catch (e:any) {
+    switch (e.message) {
+      case "Campos vazios, Favor preencha":
+        break;
+      default:
+        res.status(500).send({ message: e.sqlMessage || e.message })
+        break;
+    }
+  }
+})
+
 // esse endPoint edita um usuário
 app.put("/editUSer/:id", async (req, res): Promise<void> => {
   try {
@@ -87,8 +104,8 @@ app.put("/editUSer/:id", async (req, res): Promise<void> => {
     let nickName = req.body.nickName;
     let email = req.body.email;
 
-    if(!name && !nickName && !email){
-      throw new Error("Campos vazios, Favor preencha para fazer a editação")
+    if (!name && !nickName && !email) {
+      throw new Error("Campos vazios, Favor preencha para fazer a editação");
     }
     await connection("TodoListUser")
       .update({
@@ -98,16 +115,15 @@ app.put("/editUSer/:id", async (req, res): Promise<void> => {
       })
       .where({ id: req.params.id });
     res.status(201).send({ message: "Usuario editado com sucesso!" });
-  } catch (e:any) {
+  } catch (e) {
     switch (e.message) {
       case "Campos vazios, Favor preencha para fazer a editação":
-        res.status(422).send({message: e.message});
+        res.status(422).send({ message: e.message });
         break;
       default:
         res.status(500).send({ message: e.sqlMessage || e.message });
         break;
     }
-   
   }
 });
 
@@ -134,7 +150,7 @@ app.get("/user/:id", async (req, res) => {
             SELECT * FROM TodoListUser WHERE id = "${id}"
         `);
     res.status(205).send(result[0][0]);
-  } catch (e: any) {
+  } catch (e) {
     switch (e.message) {
       case "Insira um id válido":
         res.status(422).send(e.message);
@@ -148,6 +164,7 @@ app.get("/user/:id", async (req, res) => {
     }
   }
 });
+
 // esse endPoint um task espécifico, mas nao busca o nome do usuario
 app.get("/task/:id", async (req, res): Promise<void> => {
   try {
@@ -163,9 +180,8 @@ app.get("/task/:id", async (req, res): Promise<void> => {
     .join('ToDoListUser', 'task.creator_user_id')
     .where() nao sei como é on aqui
     */
-   console.log(result)
+    console.log(result);
     res.status(201).send(result[0][0]);
-    
   } catch (e) {
     switch (e.message) {
       case "":
@@ -180,18 +196,27 @@ app.get("/task/:id", async (req, res): Promise<void> => {
 });
 
 // esse endPoint retorna task criado por usuário
-app.get("/task?creatorUserId=id", async (req, res): Promise<void> => {
+app.get("/task", async (req, res): Promise<void> => {
   try {
     const id = req.query.id;
     if (!id) {
       throw new Error("Digite uma query válida");
     }
-    const result = await connection("task")
-      .select("*")
-      .from("task")
-      .where({ id: id });
+    // const result = await connection("task")
+    //   .select("*")
+    //   .from("task")
+    //   .where({ id: id });
+    // console.log(result);
+    // res.status(201).send(result); tentando fazer com queryBuild, mas nao consegui
+
+    const result = await connection.raw(`
+    SELECT t.*, tlu.name as creatorUserName
+    FROM task as t 
+    join TodoListUser as tlu
+    ON tlu.id = "${id}"
+      `);
     console.log(result);
-    res.status(201).send(result);
+    res.status(201).send(result[0][0]);
   } catch (e) {
     switch (e.message) {
       case "Digite uma query válida":
@@ -204,6 +229,35 @@ app.get("/task?creatorUserId=id", async (req, res): Promise<void> => {
     }
   }
 });
+
+//esse endPoint retorna usuario pesquisado por query
+app.get("/user", async (req, res) => {
+  try {
+    const id = req.query.id;
+    if (!id) {
+      throw new Error("Insira um id válido");
+    } else if (typeof id !== "string") {
+      throw new Error("Insira um tipo de id válido");
+    }
+    const result = await connection.raw(`
+            SELECT id, nickname FROM TodoListUser WHERE id = "${id}"
+        `);
+    res.status(205).send(result[0][0]);
+  } catch (e) {
+    switch (e.message) {
+      case "Insira um id válido":
+        res.status(422).send(e.message);
+        break;
+      case "Insira um id válido":
+        res.status(401).send(e.message);
+        break;
+      default:
+        res.status(500).send({ message: e.sqlMessage || e.message });
+        break;
+    }
+  }
+});
+
 app.listen(3003, () => {
   console.log("Back end rodando na porta 3003");
 });
