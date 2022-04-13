959ccd8382b85fccf2cce9de7a3716a910a61634
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-12 22:21:47 -0300

    subindo exercicio 4

--- modulo4/filtros-ordenacao-paginacao/src/endpoints/endPoint.ts
+++ modulo4/filtros-ordenacao-paginacao/src/endpoints/endPoint.ts
@@ -64,3 +64,48 @@ export const limitUser = async (req: Request, res: Response): Promise<void> => {
         res.status(404).send({message: e.message})
     }
 }
+
+/*
+Crie um último endpoint que possua todas as funcionalidades acima 
+(as duas filtragens, a ordenação e a paginação). 
+Atribua valores padrão para filtragem, ordenação e paginação para que:
+
+- Caso o usuário não forneça parâmetro de filtragem, o endpoint busque todos os users;
+- Caso o usuário não forneça parâmetro de ordenação, o endpoint ordene por **nome** em ordem **decrescente;**
+- Caso o usuário não forneça número de página, deve começar na página 1
+
+*/
+export const getUsers = async (req: Request, res: Response): Promise <void> => {
+    try {
+        let filter = req.params.filter as string
+        let order = req.query.order as string
+        const quantifyUsers = 5
+        let quantifyPages = Number(req.query.quantifyPages)
+        let orderBy = req.query.orderBy as string
+        let offset = quantifyUsers * (quantifyPages - 1)
+       
+        if(!order){
+            order = 'name'
+        }
+        if(!orderBy){
+            orderBy = 'DESC'
+        }
+        if(!filter){
+            filter = ''
+        }
+        if(quantifyPages < 1){
+            throw new Error("A Quantidade de pagina não pode ser menor que 1")
+        }
+        
+        const result = await connection('aula48_exercicio')
+        .select('*')
+        .from('aula48_exercicio')
+        .where('name', 'like', `%${filter}%`)
+        .orderBy(`${order}`, `${orderBy}`)
+        .limit(quantifyUsers)
+        .offset(offset)
+        res.status(201).send(result)
+    } catch (e: any) {
+        res.status(404).send({message: e.message})
+    }
+}
--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -1,7 +1,7 @@
-import { getAllUser, getUserName, orderUser, limitUser } from "./endpoints/endPoint";
+import { getAllUser, getUserName, orderUser, limitUser, getUsers } from "./endpoints/endPoint";
 import { app } from "./app";
+app.get('/get/users', getUsers)
 app.get('/users', getAllUser)
-
 app.get('/users/all', orderUser)
 app.get('/users/limit/:num', limitUser)
 app.get('/users/:name', getUserName)
