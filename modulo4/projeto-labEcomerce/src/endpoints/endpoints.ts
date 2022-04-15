392615fcc3090d39aaa082d8bd7196340b8118b8
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-15 18:34:11 -0300

    subindo create users e get all users

--- modulo4/projeto-labEcomerce/src/endpoints/endpoints.ts
+++ modulo4/projeto-labEcomerce/src/endpoints/endpoints.ts
@@ -1,11 +1,28 @@
-import { Request, Response } from "express"
-export const createUser = async (req: Request, res: Response) => {
+import e, { Request, Response } from "express"
+import { connection } from "../data/connection";
+const { v4: uuidv4 } = require('uuid');
+export const createUser = async (req: Request, res: Response): Promise<void> => {
     try {
-        
+        let id = uuidv4();
+        let name = req.body.name 
+        let email = req.body.email
+        let password = req.body.password
+        if(!name && !email && !password){
+            throw new Error("Campos não preenchiddos")
+        }
+        const newUser = {
+            id,
+            name,
+            email,
+            password
+        }
+        const result = await connection('labecommerce_users')
+        .insert(newUser)
+        res.status(201).send(result)
     } catch (e: any) {
         switch (e.message) {
-            case "value":
-                res.status(400).send()
+            case "Campos não preenchiddos":
+                res.status(422).send({message: e.message})
                 break;
             default:
                 res.status(500).send({message: e.sqlMessage || e.message})
@@ -16,6 +33,7 @@ export const createUser = async (req: Request, res: Response) => {
 
 export const createProduct = async (req: Request, res: Response) => {
     try {
+        const id = uuidv4();
         
     } catch (e: any) {
         switch (e.message) {
@@ -37,6 +55,24 @@ export const getAllProduct = async (req: Request, res: Response) => {
             case "value":
                 res.status(400).send()
                 break;
+            default:
+                res.status(500).send({message: e.sqlMessage || e.message})
+                break;
+        }
+    }
+}
+
+export const getAllUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const result = await connection('labecommerce_users')
+        .select('*').from('labecommerce_users')
+        res.status(201).send(result)
+    } catch (e: any) {
+        switch (e.message) {
+            case "value":
+                
+                break;
+        
             default:
                 res.status(500).send({message: e.sqlMessage || e.message})
                 break;
--- modulo4/projeto-labEcomerce/src/index.ts
+++ modulo4/projeto-labEcomerce/src/index.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
-import { createProduct, createUser, getAllProduct } from "./endpoints/endpoints";
+import { createProduct, createUser, getAllProduct, getAllUser } from "./endpoints/endpoints";
 
 app.post('/users', createUser)
 
 app.post('/products', createProduct)
 
+app.get('/users', getAllUser)
 app.get('/users/:user_id/purchases', getAllProduct)
