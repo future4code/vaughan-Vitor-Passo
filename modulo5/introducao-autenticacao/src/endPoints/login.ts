2992322907dc353583fe99b0df502446ba06185a
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-25 18:38:12 -0300

    subindo end point de login

--- modulo5/introducao-autenticacao/src/endPoints/login.ts
+++ modulo5/introducao-autenticacao/src/endPoints/login.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from "express";
 import { connection } from "../data/connection";
+import { Authentication } from "../services/Authentication";
+import getEmail from "./getEmail";
 
-async function login(req: Request, res: Response):Promise<void> {
+async function login(req: Request, res: Response): Promise<void> {
     try {
-        const {email} = req.query
-        if(!email ){
+        const {email, password} = req.body
+        if(!email || !password){
             res.statusCode = 422
-            throw new Error("Por favor, preencha os campos 'name' e 'password' ");   
+            throw new Error("Por favor, preencha os campos 'email' e 'password' ");
         }
-        const [result] = await connection('usuarios')
-        .select('*').where({email})
-        res.status(200).send(result)
         
-    } catch (error:any) {
+        const user = await getEmail(email)
+        if(user.password !== password){
+            throw new Error("Senha errada!");
+        }
+        const authentication = new Authentication
+        const generationToken = authentication.generationToken
+        const token = generationToken({
+            id: user.id
+        });
+        res.status(200).send({token: token})
+        
+    } catch (error: any) {
         if(res.statusCode === 200){
             res.status(500).send({message: "Internal server error"})
         }else{
@@ -20,4 +30,5 @@ async function login(req: Request, res: Response):Promise<void> {
         }
     }
 }
+
 export default login
--- modulo5/introducao-autenticacao/src/index.ts
+++ modulo5/introducao-autenticacao/src/index.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
 import createUser from "./endPoints/createUser";
-import login from "./endPoints/login";
-
+import getUserByEmail from "./endPoints/getUserByEmail";
+import login from "./endPoints/login"
 app.post('/user/signup', createUser)
-app.get('/user/login', login)
+app.post('/user/login', login)
+app.get('/getUser/email', getUserByEmail)
